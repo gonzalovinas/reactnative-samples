82f1b16b230e808cc846f6675db25ede919d6007
Gonzalo Viñas <gonzalo.vinas@gmail.com>
2018-02-26 13:09:51 -0300

    ..

--- samples/FooterTabWithIconsWithUserProfile.js
+++ samples/FooterTabWithIconsWithUserProfile.js
@@ -2,6 +2,62 @@ import React, { Component } from 'react';
 
 import { Badge, H1, Container, Left, Thumbnail, Body, Title, Right, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 export default class FooterTabWithIconsWithUserProfile extends Component {
+
+  constructor() {
+    super();
+    this.state = {selectedTab: null};
+  }
+
+  renderSelectedTab () {
+    switch(this.state.selectedTab) {
+        case 'elemento1':
+          return (
+             <Content>
+                 <Text style={{textAlign: 'center'}}>
+                  <H1>Elemnto 1</H1>
+                 </Text>
+             </Content>
+         );
+
+       case 'elemento2':
+         return (
+            <Content>
+                <Text style={{textAlign: 'center'}}>
+                 <H1>Elemnto 2</H1>
+                </Text>
+            </Content>
+        );
+
+      case 'elemento3':
+        return (
+           <Content>
+               <Text style={{textAlign: 'center'}}>
+                <H1>Elemnto 3</H1>
+               </Text>
+           </Content>
+       );
+
+     case 'elemento4':
+       return (
+          <Content>
+              <Text style={{textAlign: 'center'}}>
+               <H1>Elemnto 4</H1>
+              </Text>
+          </Content>
+      );
+
+    default:
+         return (
+            <Content>
+                <Thumbnail source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/AdamWestCCJul2011.jpg/220px-AdamWestCCJul2011.jpg'}}  style={{ width: 200, height: 200, borderRadius: 200/2}}  />
+                <Text style={{textAlign: 'center'}}>
+                 <H1>Bienvenido!</H1>
+                </Text>
+            </Content>
+        );
+    }
+  }
+
   render() {
     return (
       <Container>
@@ -22,48 +78,28 @@ export default class FooterTabWithIconsWithUserProfile extends Component {
                   </Button>
             </Right>
       </Header>
-      <Text></Text>
-        <Text></Text>
-          <Text></Text>
-            <Text></Text>
-              <Text></Text>
-                <Text></Text>
-                  <Text></Text>
-                    <Text></Text>
-                      <Text></Text>
-                        <Text></Text>
-                          <Text></Text>
-                            <Text></Text>
-                              <Text></Text>
-                                <Text></Text>
-                                  <Text></Text>
-      <Container style={{alignItems: 'center'}}>
-      <Content >
-      <Thumbnail source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/AdamWestCCJul2011.jpg/220px-AdamWestCCJul2011.jpg'}}  style={{ width: 200, height: 200, borderRadius: 200/2}}  />
-      <Text style={{textAlign: 'center'}}>
-      <H1>Bienvenida!</H1>
-      </Text>
-      </Content>
 
+      <Container style={{alignItems: 'center'}}>
+
+          {this.renderSelectedTab()}
 
       </Container>
-
         <Footer>
           <FooterTab  >
-            <Button vertical >
+            <Button vertical active={this.state.selectedTab==='elemento1'} onPress={() => this.setState({selectedTab: 'elemento1'})}>
               <Icon name="apps" />
               <Text style={{fontSize: 8}}>Elemento 1</Text>
             </Button>
-            <Button vertical>
+            <Button vertical active={this.state.selectedTab==='elemento2'} onPress={() => this.setState({selectedTab: 'elemento2'})}>
               <Icon name="camera" />
               <Text style={{fontSize: 8}}>Elemento 2</Text>
             </Button>
-            <Button vertical  active badge>
+            <Button vertical badge active={this.state.selectedTab==='elemento3'} onPress={() => this.setState({selectedTab: 'elemento3'})}>
               <Badge ><Text>51</Text></Badge>
               <Icon active name="navigate" />
-              <Text style={{fontSize: 8}}>Elemento 4</Text>
+              <Text style={{fontSize: 8}}>Elemento 3</Text>
             </Button>
-            <Button vertical >
+            <Button vertical active={this.state.selectedTab==='elemento4'} onPress={() => this.setState({selectedTab: 'elemento4'})}>
               <Icon name="person" />
               <Text style={{fontSize: 8}}>Ajustes</Text>
             </Button>
