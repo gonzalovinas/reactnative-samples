ebd8f7bed98aad91494ca983cdf63eb73e9b3517
GonZa <gonzalo.vinas@gmail.com>
2018-06-07 07:55:50 -0300

    ..

--- samples/bundle-ios.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-react-native bundle --platform ios --entry-file index.js --bundle-output ios/samples/main.jsbundle
--- samples/bundle.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-react-native bundle --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
