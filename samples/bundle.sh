23441e14c805d8803316bc9e65c5606a50cb72d2
GonZa <gonzalo.vinas@gmail.com>
2018-06-08 12:37:23 -0300

    ..

--- samples/bundle.sh
+++ samples/bundle.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 react-native bundle --platform android --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle
+cd android && ./gradlew assembleRelease
+cd ..
+
